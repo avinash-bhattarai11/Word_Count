5235437daf86c143b7c023798ae460780ccc3a68
avinash <avinashbhattarai43@gmail.com>
2021-11-17 10:43:30 +0545

    This is Word_Count project

---
+++ Word_Count/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+import operator 
+def home(request):
+    return render(request,'home.html')
+
+
+def count(request):
+    fulltext= request.GET['fulltext']
+    wordlist =  fulltext.split()
+
+    WordDictionary={}
+
+    for word in wordlist:
+        if word in WordDictionary:
+            #increase 
+            WordDictionary[word] += 1
+        else:
+            WordDictionary[word]= 1
+    sortedwords=sorted(WordDictionary.items(), key=operator.itemgetter(1),reverse=True)
+           
+
+    return render(request,'count.html',{'fulltext':fulltext,'count':len(wordlist),'sortedwords':sortedwords})
